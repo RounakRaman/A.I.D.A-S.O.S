2106988a887db756cdcf23d2f9dc33e4a57cd981
Rounak Raman <raman.rounak@gmail.com>
2023-07-08 02:11:02 +0530

    Solved Module Not found error for Deep Translator

---
+++ deep_translator/validate.py
@@ -0,0 +1,28 @@
+__copyright__ = "Copyright (C) 2020 Nidhal Baccouri"
+
+from typing import Optional
+
+from deep_translator.exceptions import NotValidLength, NotValidPayload
+
+
+def is_empty(text: str) -> bool:
+    return text == ""
+
+
+def is_input_valid(
+    text: str, min_chars: int = 0, max_chars: Optional[int] = None
+) -> bool:
+    """
+    validate the target text to translate
+    @param min_chars: min characters
+    @param max_chars: max characters
+    @param text: text to translate
+    @return: bool
+    """
+
+    if not isinstance(text, str) or text.isdigit():
+        raise NotValidPayload(text)
+    if max_chars and (not min_chars <= len(text) < max_chars):
+        raise NotValidLength(text, min_chars, max_chars)
+
+    return True
